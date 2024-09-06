1847cb2571c1eaf7643b5c49f4f2ed2f347cd05b
Dmytro Hrudilov <dmytrohrudilov@gmail.com>
2024-09-06 16:23:31 +0300

    Login/Logout test

---
+++ cypress/e2e/TestTask/LogIn_LogOutTest.cy.js
@@ -0,0 +1,31 @@
+describe('User Login/Logout Test', () => {
+
+  let TEST_EMAIL = 'grudilov24@gmail.com'
+  let TEST_PASSWORD = 'test1234'
+  
+    it.only('should log in with valid credentials', () => {
+      //LOG IN
+      // Navigate to the OpenWeather login page.
+      cy.visit('https://openweathermap.org');
+      cy.get('#hamburger > img').click();
+      cy.get('.gdpr-banner__button--dismiss').click();
+      cy.get(':nth-child(13) > a').click();
+
+      // Enter valid credentials
+      cy.get('#user_email').type(TEST_EMAIL);       
+      cy.get('#user_password').type(TEST_PASSWORD);    
+
+      //Submit the login form            
+      cy.get('.new_user > .btn').click();
+      
+      //redirecting to the homepage upon successful login
+      cy.url('').should('include', '/home');
+      
+      // Assert that the user profile icon is visible
+      cy.get('.panel-body').should('be.visible');
+
+      //LOG OUT
+      cy.get('#hamburger > img').click();
+      cy.get(':nth-child(14) > .logout').click();
+    });
+  });
